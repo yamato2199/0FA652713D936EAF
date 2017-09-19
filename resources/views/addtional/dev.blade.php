6f37178d7a9bcb6c8ad152db9716e447532da2e8
Epsiodes <episodes27@gmail.com>
2017-09-20 01:43:45 +1000

    fix person name

--- resources/views/addtional/dev.blade.php
+++ resources/views/addtional/dev.blade.php
@@ -10,7 +10,7 @@
         <div class="modal-content text-center">
         
         <script>
-            eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1;};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p;}('9.8("<0>a - c 2 + 1</0><0>b 4 - 3 5 1</0><0>7 6 - j 2 + 1</0><0>m l - e d f h g k i</0>");',23,23,'p|Programming|Design|Server|Cai|Side|Jiang|Wenze|write|document|Zoey|Le|Frontend|NOTHING|Done|except|others|disturb|working|Database|while|Wrong|Milisa'.split('|'),0,{}));
+            eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1;};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p;}('9.8("<0>a - c 2 + 1</0><0>b 4 - 3 5 1</0><0>7 6 - j 2 + 1</0><0>m l - e d f h g k i</0>");',23,23,'p|Programming|Design|Server|Cai|Side|Jiang|Wenze|write|document|Zoey|Le|Frontend|NOTHING|Done|except|others|disturb|working|Database|while|Wrong|Malisa'.split('|'),0,{}));
         </script>
         </div>
     </div>
