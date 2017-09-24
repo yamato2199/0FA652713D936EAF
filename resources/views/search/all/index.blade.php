df1e5aa8b55098de0366a8557802663e69d7501a
Le Cai <episodes27@gmail.com>
2017-09-24 14:53:39 +1000

    添加了没有找到结果的提示

--- resources/views/search/all/index.blade.php
+++ resources/views/search/all/index.blade.php
@@ -1,5 +1,21 @@
 @extends('layouts/base')
 @section('body')
+
+    <!-- seacrh -->
+    <form action="{{ route('search.all') }}" post="get">
+    
+        <div class="row" style="padding: 15px;">
+            <div class="col-md-10" style="margin: 0px; padding: 0px;">
+                <input name="keyword" class="form-control" placeholder="Search any food / shop you want"/>
+            </div>
+            <div class="col-md-2" style="margin: 0px; padding: 0px;">
+                <button class="btn btn-primary" style="width: 100%;" type="submit"><span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+        Search</button>
+            </div>
+        </div>
+    </form>
+    
+
     <div class="panel panel-default">
     <div class="panel-body">
     @if($result_shop->count())
@@ -45,7 +61,14 @@
             @endforeach
         </div>
     @endif
-
+    @if(!$result_dish->count() && !$result_shop->count())
+    <div class="text-center" style="padding:100px;">
+        <span class="glyphicon glyphicon-remove" style="font-size:100px;" aria-hidden="true"></span>
+        <p>Sorry, there is nothing found, try to search something else?</p>
+    
+    </div>
+    @endif
     </div>
     </div>
+    
 @endsection
